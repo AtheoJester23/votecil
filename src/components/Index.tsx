238b839cb6eac20239029be650048979aee9ee59
AtheoJester23 <atheojester@gmail.com>
2025-08-29 12:52:25 +0800

    feat: add student login form

--- src/components/Index.tsx
+++ src/components/Index.tsx
@@ -1,10 +1,66 @@
 import Navbar from "./Navbar";
 import Img4585 from '../assets/4585.jpg'
+import { useEffect, useState } from "react";
+
+
 const Index = () => {
+    const [datahandler, setDatahandler] = useState<any[]>([]);
+    const [nextPage, setNextPage] = useState<number>(0)
+
+    const fetchNew = (thePage: number) =>{
+        fetchData(thePage)
+            .then(item => {
+                if(!item) {
+                    setDatahandler([...datahandler, { id: "end", name: "The end" }]);
+                    return;
+                }
+
+                const newStuff = [
+                    ...datahandler,
+                    item
+                ]
+
+                setDatahandler(newStuff)
+                setNextPage(thePage + 1);
+            })
+    }
+
+    useEffect(() => {
+        fetchNew(nextPage);
+    }, [])
+
+    const fetchData = async (nextPage: number) => {
+        try {
+            let response = await fetch(`https://pokemonsimulation-7b844-default-rtdb.firebaseio.com/${nextPage}.json`)
+            if(!response.ok){
+                throw new Error(`Fetch failed: ${response.status}`)
+            }
+
+            let data = await response.json();
+
+            return data;
+        } catch (error) {
+            console.error(error)
+        }      
+    }
+
     return (  
         <div>
             <Navbar/>
             <img src={Img4585} alt="Front Page Introduction" className="w-[70%] h-full p-5 mx-auto"/>
+        
+            <button onClick={() => fetchNew(nextPage + 3)} className="bg-green-500 text-white font-bold px-5 py-2 rounded -translate-y-0.5 hover:translate-none duration-150 cursor-pointer">Fetch</button>
+        
+            {datahandler && (
+                datahandler.map(item => (
+                    <div key={item.id}>
+                        {item.name}
+                    </div>
+                ))
+            )}
+
+            {/* <p>{datahandler}</p> */}
+
         </div>
     );
 }
--- src/components/Login/Student.tsx
+++ src/components/Login/Student.tsx
@@ -1,7 +1,38 @@
+import stuLogPic from '../../assets/6974903_4428.jpg'
+
 const Student = () => {
     return (  
         <div className="studentLogin">
-            <h1>Student Login Page</h1>
+            <div className="loginContainer">
+                <div className="pictureContainer">
+                    <img src={stuLogPic} alt="Just an art for login" width="85%"/>
+                </div>
+
+                <div className='stuLogCont'>
+                    <form className='stuLogForm'>
+                        <h1>Login as a Student User</h1>
+                    
+                        <div className='flex flex-col w-full gap-3'>
+                            <div className='flex flex-col gap-2'>
+                                <label htmlFor="stuNum">Student Number</label>
+                                <input type="text" name='stuNum' id='stuNum' className='textInput' placeholder='Ex. 2025500110'/>
+                            </div>
+
+                            <div className='flex flex-col gap-2'>
+                                <label htmlFor="stuPass">Password</label>
+                                <input type="password" name='stuPass' id='stuPass' className='textInput' placeholder="*********"/>
+                            
+                                <div className='flex gap-2'>
+                                    <input type="checkbox" />
+                                    <label htmlFor="showPass">Show password</label>
+                                </div>
+                            
+                                <button className='bg-[rgb(28,68,120)] text-white rounded py-2'>Login</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 }
